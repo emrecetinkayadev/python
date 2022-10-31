b3a7ee913a9811463d12dfb01f9ddfa8d3da4794
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-10-31 15:38:18 +0300

    ch1-1

--- python-course-documents/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/definition_start.py
+++ python-course-documents/Ex_Files_Python_Object_Programming/Exercise Files/Ch 1/definition_start.py
@@ -3,9 +3,15 @@
 
 
 # TODO: create a basic class
+class Book():
+    def __init__(self, name) -> None:
+        self.name = name
+        
 
 
 # TODO: create instances of the class
-
+b1 = Book("Alice in Wonderland")
+b2 = Book("Lord of the Rings")
 
 # TODO: print the class and property
+print(f'Book1 :{b1.name} Class Name: {b1.__ne__}')
