3276311dc124d5f9599ecd8e96e7124c912756df
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-11-04 15:08:01 +0300

    @property decorator added.

--- python-decorator-tasks/property-decorator.py
+++ python-decorator-tasks/property-decorator.py
@@ -2,13 +2,24 @@ class Employee:
     def __init__(self, first, last) -> None:
         self.first = first
         self.last = last
-        self.email = f"{first}.{last}@email.com"
-        
+
+    @property
+    def email(self):
+        return f'{self.first}.{self.last}@email.com'
+
+    @property
     def fullname(self):
         return f'{self.first} {self.last}'
     
+    
+
+
 emp_1 = Employee('John', "Smith")
+
+emp_1.first = 'Jim'
+
+# emp_1.fullname = 'Corey Schafer'
 
 print(emp_1.first)
 print(emp_1.email)
-print(emp_1.fullname())
+print(emp_1.fullname)
