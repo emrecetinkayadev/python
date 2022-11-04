e63ffcf551394d429280c4004324fc61224d4549
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-11-04 15:14:00 +0300

    setter and deleter decorators added.

--- python-decorator-tasks/property-decorator.py
+++ python-decorator-tasks/property-decorator.py
@@ -10,16 +10,29 @@ class Employee:
     @property
     def fullname(self):
         return f'{self.first} {self.last}'
+
+    @fullname.setter
+    def fullname(self, name: str):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
     
-    
+    @fullname.deleter
+    def fullname(self):
+        print(f'{self.first} {self.last} deleted.')
+        self.first = None
+        self.last = None
 
 
 emp_1 = Employee('John', "Smith")
 
 emp_1.first = 'Jim'
 
-# emp_1.fullname = 'Corey Schafer'
+emp_1.fullname = 'Corey Schafer'
 
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
+del emp_1.fullname
+print(emp_1.fullname)
+
