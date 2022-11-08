6106f7b07e7ff639bb4b09f64ffd63630cc6105f
Emre Cetinkaya <emrecetinkayadev@gmail.com>
2022-11-08 11:13:26 +0300

    python oop task 6 part 1

--- python-oop-tasks/task6.py
+++ python-oop-tasks/task6.py
@@ -11,12 +11,47 @@ to the currency or even delete.
 
 '''
 
+import functools
 
-class Money():
+# @functools.total_ordering
+
+
+class Money(object):
     def __init__(self, value, name) -> None:
         self.value = value
         self.name = name
-        
-        
-    def comparison(self):
-        pass
+
+    def __add__(self, other_obj):
+        if isinstance(other_obj, Money):
+            return Money(self.value + other_obj.value, self.name)
+        else:
+            raise ValueError(f"{other_obj} is not a Money type.")
+
+    def __sub__(self, other_obj):
+        if isinstance(other_obj, Money):
+            return Money(self.value + other_obj.value, self.name)
+        else:
+            raise ValueError(f"{other_obj} is not a Money type.")
+
+    def __repr__(self):
+        return f"Money({self.value}, '{self.name}')"
+
+    def __getattr__(self, value):
+        self.value = value
+
+
+usd = Money(10, "usd")
+usd_t = Money(20, "usdt")
+
+usd_new = usd + usd_t
+usd_new.exchange_rate = 0.89
+
+usd_new2 = usd - usd_t
+
+# print(dir(usd_new))
+print(usd_new2)
+print(usd_new.exchange_rate)
+print(usd_new)
+
+del usd_new.exchange_rate
+print(usd_new.exchange_rate)
