31e724a25089ba36ab81083345ef79ee9acadaec
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-11-09 20:53:01 +0300

    solid open close principle added.

---
+++ python-solid/python_open_close_2.py
@@ -0,0 +1,66 @@
+
+# Open-Closed Principle
+# Objects or entities should be open for extension but closed for modification.
+
+from abc import ABC, abstractmethod
+
+
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+    # Order Class has to many responsibilities thats why we have to sperate it.
+    # def pay(self, payment_type, security_code):
+
+
+class PaymentProcessor(ABC):
+
+    @abstractmethod
+    def pay(self, order, security_code):
+        pass
+
+    # We device Payment class 2 sub classes. And Change PaymentProcessor to abstract class.
+
+
+class DebitPaymentProcessor(PaymentProcessor):
+    def pay(self, order, security_code):
+        print("Processing debit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+
+class CreditPaymentProcessor(PaymentProcessor):
+    def pay(self, order, security_code):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+
+class PaypalPaymentProcessor(PaymentProcessor):
+    def pay(self, order, security_code):
+        print("Processing paypal payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB cable", 2, 5)
+
+print(order.total_price())
+processor = PaypalPaymentProcessor()
+processor.pay(order, "0372846")
