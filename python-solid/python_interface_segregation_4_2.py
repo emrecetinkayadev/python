65b99d1b399a46305a44d2f9a29c75e4e2f13453
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-11-10 10:39:22 +0300

    interface segregation added.

--- python-solid/python_interface_segregation_4_2.py
+++ python-solid/python_interface_segregation_4_2.py
@@ -1,5 +1,5 @@
 
-# Interface Segregation Principle
+# Interface Segregation Principle (Composition)
 # A client should never be forced to implement an interface that it doesn’t use, or clients shouldn’t be forced to depend on methods they do not use.
 from abc import ABC, abstractmethod
 
@@ -24,6 +24,19 @@ class Order:
     # Order Class has to many responsibilities thats why we have to sperate it. (Single Responsiblity)
     # def pay(self, payment_type, security_code):
 
+# We create a different class and not force any other class to take any unused interface.(Composition Way - Interface Segregation)
+
+
+class SMSAuth:
+    authorized = False
+
+    def verify_code(self, code):
+        print(f"Verifying code {code}")
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
 
 class PaymentProcessor(ABC):
 
@@ -34,26 +47,13 @@ class PaymentProcessor(ABC):
     # We device Payment class 2 sub classes. And Change PaymentProcessor to abstract class. (Open Close)
 
 
-class PaymentProcessor_SMS(PaymentProcessor):
-
-    @abstractmethod
-    def auth_sms(self, code):
-        pass
-
-    # we device PaymentProcessor 2 different abstract classes. In this way we are not force any class to get any method by force.
-
-
-class DebitPaymentProcessor(PaymentProcessor_SMS):
-    def __init__(self, security_code) -> None:
+class DebitPaymentProcessor(PaymentProcessor,):
+    def __init__(self, security_code, authorizer: SMSAuth) -> None:
+        self.authorizer = authorizer
         self.security_code = security_code
-        self.verified = True
-
-    def auth_sms(self, code):
-        print(f"Verifying SMS code {code}")
-        self.verified = False
 
     def pay(self, order):
-        if not self.verified:
+        if not self.authorizer.is_authorized():
             raise Exception("Not authorized.")
         print("Processing debit payment type")
         print(f"Verifying security code: {self.security_code}")
@@ -70,17 +70,13 @@ class CreditPaymentProcessor(PaymentProcessor):
         order.status = "paid"
 
 
-class PaypalPaymentProcessor(PaymentProcessor_SMS):
-    def __init__(self, email_adress) -> None:
+class PaypalPaymentProcessor(PaymentProcessor):
+    def __init__(self, email_adress, authorizer: SMSAuth) -> None:
+        self.authorizer = authorizer
         self.email_adress = email_adress
-        self.verified = False
-
-    def auth_sms(self, code):
-        print(f"Verifying SMS code {code}")
-        self.verified = True
 
     def pay(self, order):
-        if not self.verified:
+        if not self.authorizer.is_authorized():
             raise Exception("Not authrized.")
         print("Processing paypal payment type")
         print(f"Verifying email address: {self.email_adress}")
@@ -93,6 +89,7 @@ order.add_item("SSD", 1, 150)
 order.add_item("USB cable", 2, 5)
 
 print(order.total_price())
-processor = DebitPaymentProcessor("hi@emre.com")
-processor.auth_sms("234122")
+authorizer = SMSAuth()
+processor = DebitPaymentProcessor("hi@emre.com", authorizer)
+authorizer.verify_code("2323342")
 processor.pay(order)
