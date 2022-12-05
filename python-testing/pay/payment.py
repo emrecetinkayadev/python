0bb3dc9a16cc5afe341cbc17892f0042ae76005d
emrecetinkayadev <emrecetinkayadev@gmail.com>
2022-12-05 16:44:47 +0300

    test functions %100 coverage order.py

--- python-testing/pay/payment.py
+++ python-testing/pay/payment.py
@@ -8,6 +8,10 @@ def pay_order(order: Order):
     card = input("Please enter your card number: ")
     month = int(input("Please enter the card expiry month: "))
     year = int(input("Please enter the card expiry year: "))
-    payment_processor = PaymentProcessor("6cfb67f3-6281-4031-b893-ea85db0dce20")
+    payment_processor = PaymentProcessor(
+        "6cfb67f3-6281-4031-b893-ea85db0dce20")
     payment_processor.charge(card, month, year, amount=order.total)
     order.pay()
+
+
+
